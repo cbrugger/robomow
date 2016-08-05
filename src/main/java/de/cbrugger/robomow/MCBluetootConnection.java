5cd87e2f6c6d56db2ea81bbea87fa2f7958cd365
cbrugger <cbrugger@ubuntu>
2016-08-05 21:48:13 +0200

    added

--- src/main/java/de/cbrugger/robomow/MCBluetootConnection.java
+++ src/main/java/de/cbrugger/robomow/MCBluetootConnection.java
@@ -2,9 +2,9 @@ package de.cbrugger.robomow;
 
 public class MCBluetootConnection {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
 
-	}
+    }
 
 }

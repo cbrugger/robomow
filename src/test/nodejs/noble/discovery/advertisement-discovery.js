57ce62d7a598056b1d4af5cb0e6863b94b9614fb
st4lk3r <christian@3bruggers.de>
2016-08-06 21:45:11 +0200

    fix

--- src/test/nodejs/noble/discovery/advertisement-discovery.js
+++ src/test/nodejs/noble/discovery/advertisement-discovery.js
@@ -8,6 +8,10 @@ noble.on('stateChange', function(state) {
   }
 });
 
+noble.on('characteristicsDiscover', function(characteristic) {
+	console.log('peripheral discovered (' + JSON.stringify(characteristic) + ');
+}
+
 noble.on('discover', function(peripheral) {
   console.log('peripheral discovered (' + peripheral.id +
               ' with address <' + peripheral.address +  ', ' + peripheral.addressType + '>,' +

9ec3ec8622c48e7ec6abacac3d03a9664a1d6424
st4lk3r <christian@3bruggers.de>
2016-08-06 22:53:49 +0200

    fix

--- src/test/nodejs/noble/discovery/advertisement-discovery.js
+++ src/test/nodejs/noble/discovery/advertisement-discovery.js
@@ -6,7 +6,7 @@ var allowDuplicates = false;
 noble.on('stateChange', function(state) {
   if (state === 'poweredOn') {
 	noble.startScanning();
-//    noble.startScanning(serviceUuids, allowDuplicates);
+// noble.startScanning(serviceUuids, allowDuplicates);
   } else {
     noble.stopScanning();
   }
@@ -23,35 +23,48 @@ noble.on('discover', function(peripheral) {
 		        console.log('discovered the following characteristics:');
 		        for (var i in characteristics) {
 		          console.log('  ' + i + ' uuid: ' + characteristics[i].uuid);
+		          if (characteristics[i].uuid == '2a00') {
+			          var manufacturerNameCharacteristic = characteristics[i];
+			          console.log('discovered manufacturer name characteristic');
+	
+			          manufacturerNameCharacteristic.read(function(error, data) {
+			            // data is a buffer
+			            console.log('manufacture name is: ' + data.toString('utf8'));
+			          });
+		          }
 		        }
 		      });
 		    });
 		  });
 	
-//  console.log('peripheral discovered (' + peripheral.id +
-//              ' with address <' + peripheral.address +  ', ' + peripheral.addressType + '>,' +
-//              ' connectable ' + peripheral.connectable + ',' +
-//              ' RSSI ' + peripheral.rssi + ':');
-//  console.log('\thello my local name is:');
-//  console.log('\t\t' + peripheral.advertisement.localName);
-//  console.log('\tcan I interest you in any of the following advertised services:');
-//  console.log('\t\t' + JSON.stringify(peripheral.advertisement.serviceUuids));
+// console.log('peripheral discovered (' + peripheral.id +
+// ' with address <' + peripheral.address + ', ' + peripheral.addressType + '>,'
+// +
+// ' connectable ' + peripheral.connectable + ',' +
+// ' RSSI ' + peripheral.rssi + ':');
+// console.log('\thello my local name is:');
+// console.log('\t\t' + peripheral.advertisement.localName);
+// console.log('\tcan I interest you in any of the following advertised
+// services:');
+// console.log('\t\t' + JSON.stringify(peripheral.advertisement.serviceUuids));
 //
-//  var serviceData = peripheral.advertisement.serviceData;
-//  if (serviceData && serviceData.length) {
-//    console.log('\there is my service data:');
-//    for (var i in serviceData) {
-//      console.log('\t\t' + JSON.stringify(serviceData[i].uuid) + ': ' + JSON.stringify(serviceData[i].data.toString('hex')));
-//    }
-//  }
-//  if (peripheral.advertisement.manufacturerData) {
-//    console.log('\there is my manufacturer data:');
-//    console.log('\t\t' + JSON.stringify(peripheral.advertisement.manufacturerData.toString('hex')));
-//  }
-//  if (peripheral.advertisement.txPowerLevel !== undefined) {
-//    console.log('\tmy TX power level is:');
-//    console.log('\t\t' + peripheral.advertisement.txPowerLevel);
-//  }
+// var serviceData = peripheral.advertisement.serviceData;
+// if (serviceData && serviceData.length) {
+// console.log('\there is my service data:');
+// for (var i in serviceData) {
+// console.log('\t\t' + JSON.stringify(serviceData[i].uuid) + ': ' +
+// JSON.stringify(serviceData[i].data.toString('hex')));
+// }
+// }
+// if (peripheral.advertisement.manufacturerData) {
+// console.log('\there is my manufacturer data:');
+// console.log('\t\t' +
+// JSON.stringify(peripheral.advertisement.manufacturerData.toString('hex')));
+// }
+// if (peripheral.advertisement.txPowerLevel !== undefined) {
+// console.log('\tmy TX power level is:');
+// console.log('\t\t' + peripheral.advertisement.txPowerLevel);
+// }
 
   
   

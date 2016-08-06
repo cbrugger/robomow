f1e86056711032d84a090362d045fc09b75b658d
st4lk3r <christian@3bruggers.de>
2016-08-06 22:15:43 +0200

    fix sid

--- src/test/nodejs/noble/discovery/advertisement-discovery.js
+++ src/test/nodejs/noble/discovery/advertisement-discovery.js
@@ -1,24 +1,25 @@
 var noble = require('../index');
 
+var serviceUuids = ['ff00a501d020913c123456d97200a6a6']; 
+var allowDuplicates = true; 
+
 noble.on('stateChange', function(state) {
   if (state === 'poweredOn') {
-    noble.startScanning();
+    noble.startScanning(serviceUuids, allowDuplicates);
   } else {
     noble.stopScanning();
   }
 });
 
 noble.on('discover', function(peripheral) {
-	console.log('peripheral: ' + JSON.stringify(peripheral));
-	
-//  console.log('peripheral discovered (' + peripheral.id +
-//              ' with address <' + peripheral.address +  ', ' + peripheral.addressType + '>,' +
-//              ' connectable ' + peripheral.connectable + ',' +
-//              ' RSSI ' + peripheral.rssi + ':');
-//  console.log('\thello my local name is:');
-//  console.log('\t\t' + peripheral.advertisement.localName);
-//  console.log('\tcan I interest you in any of the following advertised services:');
-//  console.log('\t\t' + JSON.stringify(peripheral.advertisement.serviceUuids));
+  console.log('peripheral discovered (' + peripheral.id +
+              ' with address <' + peripheral.address +  ', ' + peripheral.addressType + '>,' +
+              ' connectable ' + peripheral.connectable + ',' +
+              ' RSSI ' + peripheral.rssi + ':');
+  console.log('\thello my local name is:');
+  console.log('\t\t' + peripheral.advertisement.localName);
+  console.log('\tcan I interest you in any of the following advertised services:');
+  console.log('\t\t' + JSON.stringify(peripheral.advertisement.serviceUuids));
 
   var serviceData = peripheral.advertisement.serviceData;
   if (serviceData && serviceData.length) {
@@ -36,6 +37,8 @@ noble.on('discover', function(peripheral) {
     console.log('\t\t' + peripheral.advertisement.txPowerLevel);
   }
 
+  
+  
   console.log();
 });
 
